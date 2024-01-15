7516214e302d44d335ddf7be9c15b3858dba3d9c
Omar Faruk <omarfaruk65142@gmail.com>
2024-01-15 13:20:28 +0600

    Implement the react-fast-marquee and complete the brand section and latest news

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import router from './Routes/Routes.jsx';
 import { RouterProvider } from 'react-router-dom';
-
+import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,13 +1,24 @@
+import "./Header.css";
 import logo from "../../../assets/logo.png";
 import moment from 'moment';
+import { Button, Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 const Header = () => {
     return (
-        <header>
-            <div className="text-center">
-                <img src={logo} alt="" />
-                <p><small>Journalism Without Fear or Favour</small></p>
-                <p>{moment().format("dddd, MMMM DD, YYYY")}</p>
-            </div>
+        <header className="header">
+            <Container>
+                <div className="text-center">
+                    <img src={logo} alt="" />
+                    <p className="fs-18 fw-400 mt-3 dark-03 "><small>Journalism Without Fear or Favour</small></p>
+                    <p className="fs-18 fw-500 fs-20 mb-4">{moment().format("dddd, MMMM DD, YYYY")}</p>
+                </div>
+                <div className="d-flex latest-news-section bg-dark-07">
+                    <Button className="button bg-pink me-1">Latest</Button>
+                    <Marquee className="dark-02 fs-18 fw-600">
+                        I can be a React component, multiple React components, or just some text.
+                    </Marquee>
+                </div>
+            </Container>
         </header>
     );
 };
