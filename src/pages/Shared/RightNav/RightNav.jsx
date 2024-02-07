33e8334259b360e878827f2ddbd0dad0e29b5497
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-08 00:47:09 +0600

    Implement the find us section

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,12 +1,14 @@
-import { FaGoogle } from "react-icons/fa6";
+import { FaGoogle, FaInstagram } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 import "./RightNav.css";
 import { Link } from "react-router-dom";
 const RightNav = () => {
     return (
         <div className="right-navbar-wrap">
             <h2 className="fs-20 fw-600 mb-3">Login With</h2>
-            <div className="right-navbar-content">
+            <div className="right-navbar-login">
 
                 <Link to="/">
                     <div className="right-nav-login-with d-flex  border border-success rounded align-items-center justify-content-center mb-2">
@@ -21,6 +23,30 @@ const RightNav = () => {
                     </div>
                 </Link>
 
+            </div>
+            <h2 className="fs-20 fw-600 mt-5">Find Us On</h2>
+            <div className="right-navbar-find-us">
+                <div className="find-us-on rounded-top d-flex  border border-success p-4 fs-16 fw-500 dark-03 align-items-center">
+                    <div className="find-us-icon text-center rounded-3">
+                        <FaFacebookF className="find-us-icon-fb" />
+                    </div>
+
+                    <span className="ms-2">Facebook</span>
+                </div>
+                <div className="find-us-on border-top-0 d-flex  border border-success p-4 fs-16 fw-500 dark-03 align-items-center">
+                    <div className="find-us-icon text-center rounded-3">
+                        <FaTwitter className="find-us-icon-twitter" />
+                    </div>
+
+                    <span className="ms-2">Twitter</span>
+                </div>
+                <div className="find-us-on border-top-0 d-flex  border border-success p-4 fs-16 fw-500 dark-03 align-items-center">
+                    <div className="find-us-icon text-center rounded-3">
+                        <FaInstagram className="find-us-icon-instagram" />
+                    </div>
+
+                    <span className="ms-2">Instagram</span>
+                </div>
             </div>
         </div>
     );
